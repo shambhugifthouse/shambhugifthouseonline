dc591c85a360c460300bfe9cdeb7502671e63504
thepranit10 <thepranit.19@gmail.com>
2026-08-04 21:25:23 +0530

    Set admin user email to shambhugifthouse1@gmail.com

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python -c "import os, django; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shambhu_pos.settings'); django.setup(); from django.contrib.auth.models import User; from apps.authentication.models import UserProfile; user, _ = User.objects.get_or_create(username='admin'); user.set_password('admin123'); user.is_staff=True; user.is_superuser=True; user.save(); profile, _ = UserProfile.objects.get_or_create(user=user); profile.role='ADMIN'; profile.save()"
+python -c "import os, django; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shambhu_pos.settings'); django.setup(); from django.contrib.auth.models import User; from apps.authentication.models import UserProfile; user, _ = User.objects.get_or_create(username='admin'); user.set_password('admin123'); user.email='shambhugifthouse1@gmail.com'; user.is_staff=True; user.is_superuser=True; user.save(); profile, _ = UserProfile.objects.get_or_create(user=user); profile.role='ADMIN'; profile.save()"
 

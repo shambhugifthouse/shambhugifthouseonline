04bcb1eab0c9025e970587bfecc9c4b57ad4c5bd
thepranit10 <thepranit.19@gmail.com>
2026-08-04 21:44:43 +0530

    Set default EMAIL_HOST_PASSWORD and improve email reset fallback

--- shambhu_pos/settings.py
+++ shambhu_pos/settings.py
@@ -262,6 +262,6 @@ EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() in ('true', '1', 'yes')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'thepranit.19@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'mhasflpopqpsuang')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'Shambhu Gift House <thepranit.19@gmail.com>')
 

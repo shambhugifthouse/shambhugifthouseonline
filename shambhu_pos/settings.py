8e32b072d5cfe91704daa283a0bcb5269a02e05b
thepranit10 <thepranit.19@gmail.com>
2026-08-05 10:26:10 +0530

    Switch Gmail SMTP to Port 465 SSL for 100% reliable cloud email delivery on Render

--- shambhu_pos/settings.py
+++ shambhu_pos/settings.py
@@ -270,8 +270,9 @@ socket.getaddrinfo = _ipv4_getaddrinfo
 # Email SMTP Configuration (Password Reset Links & Notifications)
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() in ('true', '1', 'yes')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 465))
+EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'True').lower() in ('true', '1', 'yes')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'False').lower() in ('true', '1', 'yes')
 EMAIL_HOST_USER = 'shambhugifthouseonline@gmail.com'
 EMAIL_HOST_PASSWORD = 'iwiouzpbdukcoaau'
 DEFAULT_FROM_EMAIL = 'Shambhu Gift House <shambhugifthouseonline@gmail.com>'
